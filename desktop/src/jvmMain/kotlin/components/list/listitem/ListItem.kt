2b7e8969601daafcf092c31a721d39517d34cc36
Zsolt Oszlányi <zsolt.oscar@gmail.com>
2023-06-06 19:24:09 +0200

    optimized imports

--- desktop/src/jvmMain/kotlin/components/list/listitem/ListItem.kt
+++ desktop/src/jvmMain/kotlin/components/list/listitem/ListItem.kt
@@ -1,20 +1,10 @@
 package components.list.listitem
 
 import androidx.compose.desktop.ui.tooling.preview.Preview
-import androidx.compose.foundation.background
-import androidx.compose.foundation.layout.Box
-import androidx.compose.foundation.layout.Column
-import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.graphics.Color
 import common.preview.PreviewContainer
 import io.kanro.compose.jetbrains.expui.control.Label
-import io.kanro.compose.jetbrains.expui.style.LocalAreaColors
-import io.kanro.compose.jetbrains.expui.theme.DarkTheme
-import io.kanro.compose.jetbrains.expui.theme.Theme
-import io.kanro.compose.jetbrains.expui.window.JBWindow
 
 enum class SelectedState {
     NotSelected,
