06afd736edba8650be02eb04f81687ed7d895f16
Xover2 <64524349+Xover2@users.noreply.github.com>
2023-06-06 22:23:05 +0200

    Updated DefaultListItemBody implementation

--- desktop/src/jvmMain/kotlin/components/list/listitem/DefaultListItemBody.kt
+++ desktop/src/jvmMain/kotlin/components/list/listitem/DefaultListItemBody.kt
@@ -1,8 +1,14 @@
 package components.list.listitem
 
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.padding
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
 import io.kanro.compose.jetbrains.expui.control.Label
+import io.kanro.compose.jetbrains.expui.style.LocalAreaColors
+import io.kanro.compose.jetbrains.expui.style.LocalInactiveAreaColors
 
 @Composable
 internal fun DefaultListItemBody(
@@ -10,10 +16,19 @@ internal fun DefaultListItemBody(
     subtitle: String?
 ) {
     Column {
-        Label(title)
+        Label(
+            text = title,
+            fontSize = 14.sp
+        )
 
         subtitle?.let { subtitle ->
-            Label(subtitle)
+            Label(
+                modifier = Modifier
+                    .padding(top = 8.dp),
+                text = subtitle,
+                fontSize = 12.sp,
+                color = LocalInactiveAreaColors.current.text
+            )
         }
     }
 }
