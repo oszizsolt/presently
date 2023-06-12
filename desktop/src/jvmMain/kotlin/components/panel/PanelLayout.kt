4a7076ccdcc2e2e3dd3a03c8a4140486e72e6181
Zsolt Oszlányi <zsolt.oscar@gmail.com>
2023-06-12 22:01:45 +0200

    played with resizing panels

--- desktop/src/jvmMain/kotlin/components/panel/PanelLayout.kt
+++ desktop/src/jvmMain/kotlin/components/panel/PanelLayout.kt
@@ -1,48 +1,65 @@
 package components.panel
 
-import androidx.compose.foundation.ExperimentalFoundationApi
-import androidx.compose.foundation.background
+import androidx.compose.foundation.*
+import androidx.compose.foundation.gestures.detectDragGestures
+import androidx.compose.foundation.gestures.onDrag
 import androidx.compose.foundation.layout.*
-import androidx.compose.foundation.onClick
+import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material.Button
 import androidx.compose.material.Icon
 import androidx.compose.material.Text
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.AccountBox
+import androidx.compose.material.icons.filled.Add
 import androidx.compose.runtime.*
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.rotate
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.input.pointer.PointerIcon
+import androidx.compose.ui.input.pointer.pointerHoverIcon
+import androidx.compose.ui.input.pointer.pointerInput
 import androidx.compose.ui.layout.layout
+import androidx.compose.ui.platform.LocalDensity
+import androidx.compose.ui.unit.coerceAtLeast
+import androidx.compose.ui.unit.coerceAtMost
 import androidx.compose.ui.unit.dp
-import io.kanro.compose.jetbrains.expui.control.Label
-import io.kanro.compose.jetbrains.expui.control.ToolBarActionButton
+import io.kanro.compose.jetbrains.expui.control.*
+import io.kanro.compose.jetbrains.expui.style.AreaColors
+import io.kanro.compose.jetbrains.expui.style.LocalErrorAreaColors
+import io.kanro.compose.jetbrains.expui.style.LocalHoverAreaColors
 
 
 @OptIn(ExperimentalFoundationApi::class)
 @Composable
 fun PanelLayout(
     modifier: Modifier = Modifier,
-    mainPanelContent:  @Composable () -> Unit
-){
+    mainPanelContent: @Composable () -> Unit
+) {
     var leftPanelSelectedItem: Int? by remember { mutableStateOf(null) }
 
     Row(modifier = modifier) {
         // 1. Left side panel (narrow)
-        Column(modifier = Modifier
-            .fillMaxHeight()
-            .background(color = Color.DarkGray)
+        Column(
+            modifier = Modifier
+                .fillMaxHeight()
+                .background(color = Color.DarkGray)
         ) {
             PanelItem(modifier = Modifier
                 .vertical()
                 .rotate(-90f),
                 leftPanelSelectedItem == 0,
                 {
-                    leftPanelSelectedItem = if (leftPanelSelectedItem == 0) { null } else { 0 }
+                    leftPanelSelectedItem = if (leftPanelSelectedItem == 0) {
+                        null
+                    } else {
+                        0
+                    }
                 }
-            ){
-                Icon(modifier = Modifier
-                    .rotate(90f),
+            ) {
+                Icon(
+                    modifier = Modifier
+                        .rotate(90f),
                     imageVector = Icons.Default.AccountBox,
                     contentDescription = "",
                     tint = Color.White
@@ -53,16 +70,22 @@ fun PanelLayout(
                     color = Color.White
                 )
             }
+
             PanelItem(modifier = Modifier
                 .vertical()
                 .rotate(-90f),
                 leftPanelSelectedItem == 1,
                 {
-                    leftPanelSelectedItem = if (leftPanelSelectedItem == 1) { null } else { 1 }
+                    leftPanelSelectedItem = if (leftPanelSelectedItem == 1) {
+                        null
+                    } else {
+                        1
+                    }
                 }
-            ){
-                Icon(modifier = Modifier
-                    .rotate(90f),
+            ) {
+                Icon(
+                    modifier = Modifier
+                        .rotate(90f),
                     imageVector = Icons.Default.AccountBox,
                     contentDescription = "",
                     tint = Color.White
@@ -77,11 +100,16 @@ fun PanelLayout(
                 .rotate(-90f),
                 leftPanelSelectedItem == 2,
                 {
-                    leftPanelSelectedItem = if (leftPanelSelectedItem == 2) { null } else { 2 }
+                    leftPanelSelectedItem = if (leftPanelSelectedItem == 2) {
+                        null
+                    } else {
+                        2
+                    }
                 }
-            ){
-                Icon(modifier = Modifier
-                    .rotate(90f),
+            ) {
+                Icon(
+                    modifier = Modifier
+                        .rotate(90f),
                     imageVector = Icons.Default.AccountBox,
                     contentDescription = "",
                     tint = Color.White
@@ -95,47 +123,91 @@ fun PanelLayout(
 
         }
 
+
+        val defaultSize = with(LocalDensity.current) { 150.dp.toPx() }
+        var leftPanelWidth by remember { mutableStateOf(defaultSize) }
+
         // 2. Left side content panel
         if (leftPanelSelectedItem != null) {
             var panelColor by remember { mutableStateOf(Color.Gray) }
-            Column(modifier = Modifier
-                .fillMaxHeight()
-                .width(150.dp)
-                .background(color = panelColor)
-            ) {
-                when (leftPanelSelectedItem) {
-                    0 -> {
-                        //TODO: Call the Composable function of the corresponding panel from here
-                        Label(modifier = Modifier
-                            .background(color = Color.DarkGray),
-                            text = "Panel 1")
-                        panelColor = Color.Blue
-                    }
-                    1 -> {
-                        //TODO: Call the Composable function of the corresponding panel from here
-                        Label(modifier = Modifier
-                            .background(color = Color.DarkGray),
-                            text = "Panel 2")
-                        panelColor = Color.Cyan
-                    }
-                    2 -> {
-                        //TODO: Call the Composable function of the corresponding panel from here
-                        Label(modifier = Modifier
-                            .background(color = Color.DarkGray),
-                            text = "Panel 3")
-                        panelColor = Color.Yellow
-                    }
-                    null -> {
 
+            Column(
+                modifier = Modifier
+                    .fillMaxHeight()
+                    .width(with(LocalDensity.current) { leftPanelWidth.toDp() })
+                    .background(color = panelColor)
+            ) {
+                Box(
+                    modifier = Modifier
+                        .fillMaxSize(),
+                ) {
+                    Box(
+                        modifier = Modifier
+                            .fillMaxSize()
+                    ) {
+                        when (leftPanelSelectedItem) {
+                            0 -> {
+                                //TODO: Call the Composable function of the corresponding panel from here
+                                Label(
+                                    modifier = Modifier
+                                        .background(color = Color.DarkGray),
+                                    text = "Panel 1"
+                                )
+                                panelColor = Color.Blue
+                            }
+
+                            1 -> {
+                                //TODO: Call the Composable function of the corresponding panel from here
+                                Label(
+                                    modifier = Modifier
+                                        .background(color = Color.DarkGray),
+                                    text = "Panel 2"
+                                )
+                                panelColor = Color.Cyan
+                            }
+
+                            2 -> {
+                                //TODO: Call the Composable function of the corresponding panel from here
+                                Label(
+                                    modifier = Modifier
+                                        .background(color = Color.DarkGray),
+                                    text = "Panel 3"
+                                )
+                                panelColor = Color.Yellow
+                            }
+
+                            null -> {
+
+                            }
+                        }
                     }
+
+                    val minHeight = with(LocalDensity.current) { 40.dp.toPx() }
+                    val maxHeight = with(LocalDensity.current) { 300.dp.toPx() }
+
+
+                    Box(
+                        modifier = Modifier
+                            .align(Alignment.TopEnd)
+                            .width(16.dp)
+                            .fillMaxHeight()
+                            .pointerHoverIcon(PointerIcon(org.jetbrains.skiko.Cursor(java.awt.Cursor.W_RESIZE_CURSOR)))
+                            .onDrag {
+                                leftPanelWidth = (leftPanelWidth + it.x)
+                                    .coerceAtLeast(minHeight)
+                                    .coerceAtMost(maxHeight)
+                            }
+
+                    )
                 }
             }
         }
 
         // 3. Main panel
-        Column(modifier = Modifier
-            .fillMaxHeight()
-            .weight(1F)
+        Column(
+            modifier = Modifier
+                .fillMaxHeight()
+                .weight(1F)
         ) {
             mainPanelContent()
         }
